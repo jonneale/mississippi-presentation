61b8709023f9bcad47f7a5ec291f8e0df173995d
Jon Neale <jon.neale@uswitch.com>
2012-10-30 17:55:35 +0000

    Added project outline

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mississippi-presentation
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/mississippi_presentation/core_test.clj
@@ -0,0 +1,7 @@
+(ns mississippi-presentation.core-test
+  (:use clojure.test
+        mississippi-presentation.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
